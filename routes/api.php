cdd436c90697204cc80d30d1717b8911985f2635
DusanSistem <dusan.glisic@sistemi.rs>
2023-05-23 15:49:09 +0200

    crudRoute

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\KeyboardController;
+use App\Http\Controllers\MouseController;
+use App\Http\Controllers\PcController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,9 +24,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/employees', [EmployeeController::class, 'index']);
-
 Route::get('/employees/{id}', [EmployeeController::class, 'show']);
 Route::post('/employees', [EmployeeController::class, 'store']);
 Route::post('/employees/{id}', [EmployeeController::class, 'update']);
-Route::delete('/employeesDelete/{id}', [EmployeeController::class, 'destroy']);
+Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+
+Route::get('/equipments', [EquipmentController::class, 'index']);
+Route::get('/equipments/{id}', [EquipmentController::class, 'show']);
+Route::post('/equipments', [EquipmentController::class, 'store']);
+Route::post('/equipments/{id}', [EquipmentController::class, 'update']);
+Route::delete('/equipments/{id}', [EquipmentController::class, 'destroy']);
+
+Route::get('/keyboards', [KeyboardController::class, 'index']);
+Route::get('/keyboards/{id}', [KeyboardController::class, 'show']);
+Route::post('/keyboards', [KeyboardController::class, 'store']);
+Route::post('/keyboards/{id}', [KeyboardController::class, 'update']);
+Route::delete('/keyboards/{id}', [KeyboardController::class, 'destroy']);
+
+Route::get('/mouses', [MouseController::class, 'index']);
+Route::get('/mouses/{id}', [MouseController::class, 'show']);
+Route::post('/mouses', [MouseController::class, 'store']);
+Route::post('/mouses/{id}', [MouseController::class, 'update']);
+Route::delete('/mouses/{id}', [MouseController::class, 'destroy']);
+
+Route::get('/pcs', [PcController::class, 'index']);
+Route::get('/pcs/{id}', [PcController::class, 'show']);
+Route::post('/pcs', [PcController::class, 'store']);
+Route::post('/pcs/{id}', [PcController::class, 'update']);
+Route::delete('/pcs/{id}', [PcController::class, 'destroy']);
 
