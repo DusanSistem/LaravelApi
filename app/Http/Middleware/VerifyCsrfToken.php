e875219ecee3885fe4372ad6cede96666aaee9ba
DusanSistem <dusan.glisic@sistemi.rs>
2023-06-15 13:09:53 +0200

    docker and cros

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://127.0.0.1:8000/api/login',
-        'http://127.0.0.1:8000/api/logout',
-        'http://127.0.0.1:8000/api/employees',
-        'http://127.0.0.1:8000/api/employees/*'
+        '/api/login',
+        '/api/logout',
+        '/api/employees',
+        '/api/employees/*'
     ];
 }
